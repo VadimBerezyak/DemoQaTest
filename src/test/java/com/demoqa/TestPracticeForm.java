fa9c6be3201586862d38073941f95b53c05294bd
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-07-31 18:49:45 +0300

    @Beforeall added

--- src/test/java/com/demoqa/TestPracticeForm.java
+++ src/test/java/com/demoqa/TestPracticeForm.java
@@ -1,14 +1,22 @@
 package com.demoqa;
 
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class TestPracticeForm {
     @BeforeAll
     static void configure(){
-        Co
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browser = "firefox";
+        Configuration.browserSize = "1024x768";
+    }
+    @BeforeEach
+    static  void beforeTest(){
 
     }
+
 
 }
