c4317425c716aea8fe0730a09945304d51a3f961
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-07-31 22:29:51 +0300

    dateOfBirth was added

--- src/test/java/com/demoqa/TestPracticeForm.java
+++ src/test/java/com/demoqa/TestPracticeForm.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class TestPracticeForm {
@@ -22,9 +22,22 @@ public class TestPracticeForm {
         Selenide.clearBrowserCookies();
     }
         @Test
-                void StudentRegistrationFormTest(){
+        void StudentRegistrationFormTest(){
         open("automation-practice-form");
-        
+
+        $("#firstName").setValue("Vadim");
+        $("#lastName").setValue("Berezyak");
+        $("#userEmail").setValue("berez@mail.ru");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("+79999999999");
+        $("#dateOfBirthInput").click();
+        $x("//option[@value = '0']").click();
+        $x("//option[@value = '1900']").click();
+        $x("//div[@aria-label = 'Choose Monday, January 1st, 1900']").click();
+
+        $("#gender-radio-2").click();
+
+            //input[@name = 'gender']
 
         }
 
