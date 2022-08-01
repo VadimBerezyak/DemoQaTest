f82a29dffa04cbc6c5ed169f306a144e13943c4a
Berezyak Vadim <newberez@gmail.com>
2022-08-01 14:35:55 +0300

    code was refactored

--- src/test/java/com/demoqa/TestPracticeForm.java
+++ src/test/java/com/demoqa/TestPracticeForm.java
@@ -56,15 +56,17 @@ public class TestPracticeForm {
         $(byText("Karnal")).click();
         $("#submit").click();
         SelenideElement tableResponsive = $(".table-responsive");
-        tableResponsive.shouldHave(text("Vadim Berezyak"));
-        tableResponsive.shouldHave(text("berez@mail.ru"));
-        tableResponsive.shouldHave(text("Male"));
-        tableResponsive.shouldHave(text("7999999999"));
-        tableResponsive.shouldHave(text("01 January,1900"));
-        tableResponsive.shouldHave(text("Maths, English"));
-        tableResponsive.shouldHave(text("Sports, Reading, Music"));
-        tableResponsive.shouldHave(text("theCat.jpg"));
-        tableResponsive.shouldHave(text("USSR, Red Square"));
-        tableResponsive.shouldHave(text("Haryana Karnal"));
+        tableResponsive.shouldHave(
+                text("Vadim Berezyak"),
+                text("berez@mail.ru"),
+                text("Male"),
+                text("Male"),
+                text("01 January,1900"),
+                text("Maths, English"),
+                text("Sports, Reading, Music"),
+                text("theCat.jpg"),
+                text("USSR, Red Square"),
+                text("Haryana Karnal"),
+                text("7999999999"));
     }
 }
